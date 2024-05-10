fa925f1ef969cb5180d10d2624703ebe51f01a81
BiggerH2 <161705584+BiggerH2@users.noreply.github.com>
2024-05-11 00:34:32 +0300

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import MainContainer from "./MainContainer";
 
 function App() {
+  const [portfolio, setPortfolio] = useState([]);
+
+  const addStockToPortfolio = (stock) => {
+    setPortfolio((prevPortfolio) => [...prevPortfolio, stock]);
+  };
+
+  const removeStockFromPortfolio = (stockId) => {
+    setPortfolio((prevPortfolio) =>
+      prevPortfolio.filter((stock) => stock.id !== stockId)
+    );
+  };
+
   return (
     <div>
       <Header />
-      <MainContainer />
+      <MainContainer
+        portfolio={portfolio}
+        addStockToPortfolio={addStockToPortfolio}
+        removeStockFromPortfolio={removeStockFromPortfolio}
+      />
     </div>
   );
 }
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
-function MainContainer() {
+function MainContainer({ portfolio, addStockToPortfolio, removeStockFromPortfolio }) {
+  const [stocks, setStocks] = useState([]);
+  const [sortType, setSortType] = useState("Alphabetically");
+  const [filterType, setFilterType] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then((response) => response.json())
+      .then((data) => setStocks(data));
+  }, []);
+
+  const handleSort = (event) => {
+    setSortType(event.target.value);
+  };
+
+  const handleFilter = (event) => {
+    setFilterType(event.target.value);
+  };
+
+  const sortedStocks = [...stocks].sort((a, b) => {
+    if (sortType === "Alphabetically") {
+      return a.ticker.localeCompare(b.ticker);
+    } else if (sortType === "Price") {
+      return a.price - b.price;
+    }
+    return 0;
+  });
+
+  const filteredStocks = sortedStocks.filter((stock) => {
+    if (filterType === "") {
+      return true;
+    }
+    return stock.type === filterType;
+  });
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar handleSort={handleSort} handleFilter={handleFilter} />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer
+            stocks={filteredStocks}
+            addStockToPortfolio={addStockToPortfolio}
+          />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer
+            portfolio={portfolio}
+            removeStockFromPortfolio={removeStockFromPortfolio}
+          />
         </div>
       </div>
     </div>
